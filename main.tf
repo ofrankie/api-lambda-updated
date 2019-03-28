6b190ac56f71f5a640b850380e93fe32e26fa3a8
ofrankie <frank.olufemi@gmail.com>
2019-03-28 11:58:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,87 +41,20 @@ data "template_file" HelloWorld_api_swagger{
   template = "${file("swagger.yaml")}"
 }
 
+resource "aws_api_gateway_api_key" "HelloWorld-api-prod-key" {
+  name = "HelloWorld-api-prod-key"
+}
+
 resource "aws_api_gateway_deployment" "HelloWorld_deployment_prod" {
-  depends_on = [
-    "aws_api_gateway_method.HelloWorld_api_method",
-    "aws_api_gateway_integration.HelloWorld_api_method-integration","aws_lambda_function.HelloWorld_function"
-  ]
+ # depends_on = [
+ #   "aws_api_gateway_method.HelloWorld_api_method",
+ #   "aws_api_gateway_integration.HelloWorld_api_method-integration","aws_lambda_function.HelloWorld_function"
+ # ]
   rest_api_id = "${aws_api_gateway_rest_api.HelloWorld_api.id}"
   stage_name = "api"
 }
 
-resource "aws_cloudfront_distribution" "HelloWorld_cloudfront" {
-  depends_on = ["aws_lambda_function.HelloWorld_function"]
-  enabled  = true
-  #aliases  = ["${var.HelloWorld_domain}"]
-  origin {
-    origin_id   = "origin-api-${aws_api_gateway_deployment.HelloWorld_deployment_prod.rest_api_id }"
-    domain_name = "${aws_api_gateway_deployment.HelloWorld_deployment_prod.rest_api_id}.execute-api.${var.region}.amazonaws.com"
-    origin_path = "/api/messages"
 
-    custom_origin_config {
-      origin_protocol_policy = "https-only"
-      http_port              = "80"
-      https_port             = "443"
-      origin_ssl_protocols   = ["TLSv1","TLSv1.1","TLSv1.2"]
-    }
-     custom_header {
-       name = "x-api-key"
-       value = "${aws_api_gateway_api_key.HelloWorld-api-prod-key.value}"
-     }
-  }
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "origin-api-${aws_api_gateway_deployment.HelloWorld_deployment_prod.rest_api_id }"
-  
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    #viewer_protocol_policy = "redirect-to-https"
-    viewer_protocol_policy = "https-only"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-  ordered_cache_behavior {
-    path_pattern     = "api*"
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "origin-api-${aws_api_gateway_deployment.HelloWorld_deployment_prod.rest_api_id}"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-
-    viewer_protocol_policy = "https-only"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-  viewer_certificate {
-    cloudfront_default_certificate = true
-    #acm_certificate_arn            = "${aws_acm_certificate_validation.short_url_domain_cert.certificate_arn}"
-    ssl_support_method             = "sni-only"
-    minimum_protocol_version       = "TLSv1.1_2016"
-  }
-}
 
 output "Prod API Key" {
   value = "${aws_api_gateway_api_key.HelloWorld-api-prod-key.value}"
@@ -130,5 +63,3 @@ output "Prod API Key" {
 output "prod_url" {
   value = "https://${aws_api_gateway_deployment.HelloWorld_deployment_prod.rest_api_id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.HelloWorld_deployment_prod.stage_name}/messages"
 }
-
-
